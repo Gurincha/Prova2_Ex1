c349a4992f5bd44ad2b558d38e6fdb33904695d8
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-06-06 16:47:55 +0000

    att

---
+++ Prova2.java
@@ -0,0 +1,12 @@
+import javax.management.Query;
+
+public class Prova2{
+    public static void main(String[] args) {
+        int segundaSemana = 5;
+        int terceiraSemana = 2;
+        int quartaSemana = 7;
+        System.out.println(segundaSemana);
+        System.out.println(terceiraSemana);
+        System.out.println(quartaSemana);
+    }
+}
